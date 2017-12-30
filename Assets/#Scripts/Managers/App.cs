ce5300de142817a97151bfa937f6b6d21c6d9929
rawtoast <jim@jamesmorris.org>
2017-12-30 22:38:54 +0000

    Improved transitions

--- Assets/#Scripts/Managers/App.cs
+++ Assets/#Scripts/Managers/App.cs
@@ -1,8 +1,15 @@
-﻿using UnityEngine;
+﻿#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine;
 
 namespace Managers {
     public class App : MonoBehaviour {
         private void Awake() {
+            #if UNITY_EDITOR
+                return;
+            #endif
+            
             UnityEngine.SceneManagement.SceneManager.LoadScene("Menu");
         }
     }
